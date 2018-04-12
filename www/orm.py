505783d2460281cca3521d110ea24598d800a4df
yangshunxin <823422842@qq.com>
2018-04-13 02:21:28 +0800

    add gitigonre and orm.py but not complete

---
+++ www/orm.py
@@ -0,0 +1,61 @@
+#_*_coding:utf-8_*_
+import asyncio, logging, aiomysql
+
+def log(sql, args=()):
+    logging.info('SQL: %s'%sql)
+
+#创建数据库连接池
+@asyncio.coroutine
+def create_pool(loop, **kw):
+    logging.info('Create database connection pool....')
+    global __pool
+    __pool = yield from aiomysql.create_pool(
+        host=kw.get('host', 'localhost'),
+        port=kw.get('port', 3306),
+        user=kw['user'],
+        password=kw['password'],
+        db=kw['db'],
+        charset=kw.get('charset', 'utf8'),
+        autocommit=kw.get('autocommit', True),
+        maxsize=kw.get('maxsize', 10),
+        minsize=kw.get('minsize', 1),
+        loop=loop
+    )
+
+#执行select语句
+@asyncio.coroutine
+def select(sql, args, size=None):
+    log(sql, args)
+    global __pool
+    with (yield from __pool) as conn:
+        cur = yield from conn.cursor(aiomysql.DictCursor)
+        yield from cur.execute(sql.replace('?', '%s'), args or ())
+        if size:
+            rs = yield from cur.fetchmany(size)
+        else:
+            rs = yield from cur.fetchall()
+        yield from cur.close()
+        logging.info('rows returned:%s'%len(rs))
+        return rs
+
+#insert update delete在同一语句中
+@asyncio.coroutine
+def execute(sql, args):
+    log(sql)
+    global __pool
+    with (yield from __pool) as conn:
+        try:
+            cur = yield from conn.cursor()
+            yield from cur.execute(sql.replace('?', '%s'), args)
+            affected = cur.rowcount
+            yield from cur.close()
+        except BaseException as e:
+            raise
+        return affected
+
+class Model(dict, metaclass=ModelMetaclass):
+
+
+
+
+
